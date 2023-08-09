afe6239952b485852710d3f8a3824ee2cca9e5cb
Musa Liman <musaliman27@gmail.com>
2023-08-10 00:26:32 +0100

    respnsive update

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -8,7 +8,7 @@ const TextInput = ({ type, name, placeholder, value, onChange, className }) => {
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      className={`border rounded px-3 py-2 text-gray-700 text-sm sm:hover:ring-3 sm:ring-mustardtard ${className}`}
+      className={`border rounded px-3 py-2 text-gray-700 text-sm sm:ring-mustard sm:focus:ring-3  ${className}`}
     />
   );
 };
